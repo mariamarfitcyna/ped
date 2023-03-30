0de654a43fc712b5b1d24641d3692eb08376b1ac
Alexander Ziyatdinov <50361221+AlexanderZiyatdinov@users.noreply.github.com>
2023-03-31 02:13:08 +0500

    Final version
    - added task3
    - added task4
    - fixed some functions

--- src/main/java/Party.java
+++ src/main/java/Party.java
@@ -1,2 +1,16 @@
-public class Party {
+import java.time.LocalDate;
+
+public class Party extends Product {
+    private final int count;
+
+    public Party(String _name, Float _price, LocalDate _productionDate, int _expirationDate, int _count) {
+        super(_name, _price, _productionDate, _expirationDate);
+        count = _count;  // Количество в партии
+    }
+
+    @Override
+    public void getParameters() {
+        super.getParameters();
+        System.out.printf("Количество в партии: %s%n", count);
+    }
 }
